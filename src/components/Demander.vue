cd3d82b5c3a0e7a30b803b0b8e24190784e1873a
baboucarr <baboucarrd43@gmail.com>
2019-05-07 15:59:09 +0000

    added demander

--- src/components/Demander.vue
+++ src/components/Demander.vue
@@ -1,6 +1,68 @@
 <template>
     <div>
     <Navigation/>
+    <div class="container">
+        <h1>Demander un devis</h1>
+        <form action="">
+           <div class="flex__input"> 
+               <label for="informatique">Informatique</label>
+               <input type="checkbox" name="informatique" id="informatique">
+           </div>
+
+           <div class="flex__input">
+               <label for="photocopie">Impression - Numérisation - Photocopie</label>
+               <input type="checkbox" name="" id="">
+           </div>
+
+           <div class="flex__input">
+               <label for=""> Grand Ecran</label>
+               <input type="checkbox" name="gradnecran" id="">
+           </div>
+
+           <div class="flex__input">
+               <label for=""> Mur d'images</label>
+               <input type="checkbox" name="" id="">
+           </div>
+
+           <div class="flex__input">
+               <label for="vide">Vidéoprojection</label>
+               <input type="checkbox" name="" id="">
+           </div>
+
+           <div class="flex__input">
+               <label for="">Sonorisation</label>
+               <input type="checkbox" name="" id="">
+           </div>
+
+           <div class="flex__input">
+               <label for="">Eclairage</label>
+               <input type="checkbox" name="" id="">
+           </div>
+
+            <div class="flex__input">
+                <label for="">Assistance</label>
+                <input type="checkbox">
+            </div>
+
+            <div class="flex__input">
+                <label for="">Développement spécifique</label>
+                <input type="checkbox" name="" id="">
+            </div>
+           
+            <h5>Modalités de livraison</h5>
+            <div class="flex__input">
+                <label for="">
+Modalités de livraison
+ Je viens chercher le matériel en vos locaux</label>
+  <input type="checkbox" name="" id="">
+            </div>
+            
+            <div class="flex__input">
+                <label for="">Je désire une livraison</label>
+                <input type="checkbox" name="" id="">
+            </div>
+        </form>
+    </div>
 
     </div>
 </template>
@@ -15,5 +77,15 @@ import Navigation from '../components/Navigation';
 </script>
 
 <style lang="scss" scoped>
+  .flex__input{
+      display: flex;
+    align-items: center;
+    flex-direction: row-reverse;
+    justify-content: flex-end;
 
+  }
+
+  label{
+       padding-left: 1rem;
+  }
 </style>
