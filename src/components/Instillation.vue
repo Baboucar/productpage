a15579aeb0ef4e1a815ff9d81bcb92eb03c7736d
baboucarr <baboucarrd43@gmail.com>
2019-05-21 06:56:55 +0000

    added instillation

--- src/components/Instillation.vue
+++ src/components/Instillation.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <Navigation/>
+    <div class="grid__instillation">
     <section class="instillation container">
       <h2 class="instillation__title">Livraison et installation</h2>
       <img src="productimage/installation_evenement.jpg" alt="public">
@@ -27,17 +28,44 @@ Paris, région parisienne et Nord de la France:</h3>
 </p>
     </section>
 
+    <section class="info container">
+    <h2 class="info__title">Infos pratiques</h2>
+    <hr>
+    <p>
+        Ayant son siège social et son stock principal à Paris, Eurolocation intervient aisément sur des évènements dans la partie nord de l'hexagone. Nos équipes connaissent par coeur le Paris expo Porte de Versailles, Paris Nord Villepinte, le Carrousel du Louvre, le CNIT Paris La Défense et se déplacent régulièrement à Lille Grand Palais, à la Cité des congrès de Nantes, au Strasbourg Convention & Exhibition Centre et au Centre International de Deauville (CID).
+    </p>
+      
+      <p>
+          Vos données personnelles ne feront pas l'objet de traitement informatique et ne seront utilisées que dans le cadre du présent devis pour pouvoir vous contacter.
+      </p>
+
+      <h2>Liens utiles</h2>
+      <hr>
+       <router-link to="/demander">Demander</router-link>
+    </section>
+</div>
+<Footer/>
   </div>
 </template>
 
 <script>
 import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
 export default {
   components: {
-    Navigation
+    Navigation,
+    Footer
   }
 };
 </script>
 
 <style lang="scss" scoped>
+
+
+@media (min-width:64rem){
+  .grid__instillation{
+      display: grid;
+      grid-template-columns: 2fr 1fr;
+  }
+}
 </style>
