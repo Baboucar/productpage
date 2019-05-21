d2e8fb43824877803c459fff73ee88613eeb72f3
baboucarr <baboucarrd43@gmail.com>
2019-05-21 06:46:26 +0000

    added instillation

--- src/components/Instillation.vue
+++ src/components/Instillation.vue
@@ -1,22 +1,43 @@
 <template>
-    <div>
-   <Navigation/>
+  <div>
+    <Navigation/>
     <section class="instillation container">
-        <h2 class="instillation__title">Livraison et installation</h2>
-         <img src="productimage/installation_evenement.jpg" alt="public">
+      <h2 class="instillation__title">Livraison et installation</h2>
+      <img src="productimage/installation_evenement.jpg" alt="public">
+      <p>
+        Nos équipes techniques se chargent de la livraison, de l'installation
+        et de la mise en marche du matériel ainsi que de sa reprise à l'issue de
+        l'événement en soignant les points forts suivants:
+      </p>
+      <ul>
+        <li>Ponctualité</li>
+        <li>Compétence</li>
+        <li>Amabilité</li>
+        <li>Disponibilité</li>
+      </ul>
+         <h3>Eurolocation Informatique couvre non seulement toute la France mais intervient aussi dans l'Europe entière.</h3>
+         <p>
+             Avec nos agences dans le nord et le sud de la France, nous intervenons sur Paris et sa région et les principales métropoles telles que Lille, Nantes, Strasbourg, Nice, Cannes, Monaco, etc.
+Nous nous déplaçons aussi régulièrement sur le continent Européen afin de répondre aux exigences de notre clientèle. 
+         </p>
+         <h3>
+Paris, région parisienne et Nord de la France:</h3>
+<p>
+    Ayant son siège social et son stock principal à Paris, Eurolocation intervient aisément sur des évènements dans la partie nord de l'hexagone. Nos équipes connaissent par coeur le Paris expo Porte de Versailles, Paris Nord Villepinte, le Carrousel du Louvre, le CNIT Paris La Défense et se déplacent régulièrement à Lille Grand Palais, à la Cité des congrès de Nantes, au Strasbourg Convention & Exhibition Centre et au Centre International de Deauville (CID).
+</p>
     </section>
-    </div>
+
+  </div>
 </template>
 
-<script> 
-import Navigation from '../components/Navigation';
-    export default {
-        components:{
-            Navigation
-        }
-    }
+<script>
+import Navigation from "../components/Navigation";
+export default {
+  components: {
+    Navigation
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
